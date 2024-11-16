0418bc7308924975cf5b37fcd4cb550dd267c557
RAJA-adam-666 <adamardiatama0@gmail.com>
2024-11-16 04:04:25 +0000

    test lanjutan

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
+#Struktur IF
 nilai_akhir = int(input("masukan nilai : "))
-if nilai_akhir  >=60 :   
-   keterangan ="lulus"
+if nilai_akhir  >=100 :   
+   keterangan ="asu"
    print(f"anda dinyatakan : {keterangan} dengan nilai :  {nilai_akhir}" )
+elif nilai_akhir  >=60 :   
+   keterangan ="kanjut"
+   print(f"anda dinyatakan : {keterangan} dengan nilai :  {nilai_akhir}" )
+   
 else :
    keterangan ="tidak lulus"
    print(f"anda dinyatakan : {keterangan} dengan nilai :  {nilai_akhir}" )
