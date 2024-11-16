c5dfdc065bbd64343bc5422f50c2b54eff9ca5ae
RAJA-adam-666 <adamardiatama0@gmail.com>
2024-11-16 03:46:12 +0000

    test

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-
+nilai_akhir = int(input("masukan nilai : "))
+if nilai_akhir  >=60 :   
+   keterangan ="lulus"
+   print(f"anda dinyatakan : {keterangan} dengan nilai :  {nilai_akhir}" )
+else :
+   keterangan ="tidak lulus"
+   print(f"anda dinyatakan : {keterangan} dengan nilai :  {nilai_akhir}" )
