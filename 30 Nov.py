300ca04dd4ad0515213ae0165d7a4493ec6b1224
RAJA-adam-666 <adamardiatama0@gmail.com>
2024-11-30 04:48:51 +0000

    30 Nov.py

--- 30 Nov.py
+++ 30 Nov.py
@@ -10,3 +10,36 @@ for i in range(0,n):
     
 print("data list:",number_list)
 print("jumlah:",jum)
+
+#manipulasi perulangan untuk tipe data dictionary
+transaction_data ={
+    "id" : 1,
+    "source_country" : "United Kingdom",
+    "target_country" : "Indonesia",
+    "currency" : "GBP",
+    "amount" : 1000.00,
+    "target_currency" : "Rp",
+    "fx_rate" : 19972.91,
+    "platform" : "mobile",
+    
+}
+print(transaction_data)
+print()
+for key in transaction_data:
+    print(key,";",transaction_data[key])
+    
+#neested loop
+for i in range(1,5,1):
+    for j in range(0,3,1):
+        print(i*j, end=" ")
+    print()
+
+#buat pola segitiga dengan perulangan
+k=1
+baris=int(input('tentukan baris dari segitiga:'))
+for i in range(1,baris+1):
+    print(' ' * (baris - i),end="")
+    for j in range(1,i+1):
+        print(k, end=" ")
+        k=k+1
+    print()
